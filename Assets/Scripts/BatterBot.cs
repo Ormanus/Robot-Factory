e2b3da0ddc47b8cf06a3c3f8159fa2a920fb6f09
dvrisa <38985578+Riku-S@users.noreply.github.com>
2023-07-09 17:15:32 +0300

    Give batter bot a reaction distance before it charges

--- Assets/Scripts/BatterBot.cs
+++ Assets/Scripts/BatterBot.cs
@@ -23,6 +23,8 @@ public class BatterBot : MonoBehaviour
     float timeSinceBatting = 0f;
     float timeSinceCharging = 0f;
 
+    const float reactionDistance = 5f;
+
     enum BatterState
     {
         Moving,
@@ -35,50 +37,71 @@ public class BatterBot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        hedgehog = FindObjectOfType<HedgehogController>();
         robotMovement = GetComponent<RobotMovement>();
         robotMovement.movementSpeed= speed;
         batCollider = BatObject.transform.GetComponent<Collider2D>();
-        hedgehogCollider = hedgehog.GetComponent<Collider2D>();
-        hedgehogRB = hedgehogCollider.attachedRigidbody;
+    }
+
+    void FindHedgehog()
+    {
+        if (hedgehog == null)
+        {
+            hedgehog = FindObjectOfType<HedgehogController>();
+            if (hedgehog != null)
+            {
+                hedgehogCollider = hedgehog.GetComponent<Collider2D>();
+                hedgehogRB = hedgehogCollider.attachedRigidbody;
+            }
+        }
+    }
+
+    float Dist2D(Vector3 a, Vector3 b)
+    {
+        return (new Vector2(a.x, a.y) - new Vector2(b.x, b.y)).magnitude;
     }
 
     void Move()
     {
         if (hedgehog != null)
         {
-            robotMovement.SetTarget(hedgehog.transform.position);
-            Vector3 hedgeHogPosition = new Vector3(hedgehog.transform.position.x, hedgehog.transform.position.y, transform.position.z);
-            //    transform.position = Vector3.MoveTowards(transform.position, hedgeHogPosition, speed * Time.deltaTime);
-            //    transform.localEulerAngles = new Vector3(0f, 0f, Vector2.SignedAngle(Vector2.up, hedgeHogPosition - transform.position));
-
-            Wings.transform.localEulerAngles = new Vector3(0, 0, 0);
-
-            if ((hedgeHogPosition - transform.position).magnitude < chargeDistance)
+            if (Dist2D(hedgehog.transform.position, transform.position) < reactionDistance)
             {
-                state = BatterState.Charging;
-                timeSinceCharging = 0;
-                robotMovement.movementSpeed = speed / 2;
+                robotMovement.SetTarget(hedgehog.transform.position);
+                Vector3 hedgeHogPosition = new Vector3(hedgehog.transform.position.x, hedgehog.transform.position.y, transform.position.z);
+                //    transform.position = Vector3.MoveTowards(transform.position, hedgeHogPosition, speed * Time.deltaTime);
+                //    transform.localEulerAngles = new Vector3(0f, 0f, Vector2.SignedAngle(Vector2.up, hedgeHogPosition - transform.position));
+
+                Wings.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+                if ((hedgeHogPosition - transform.position).magnitude < chargeDistance)
+                {
+                    state = BatterState.Charging;
+                    timeSinceCharging = 0;
+                    robotMovement.movementSpeed = speed / 2;
+                }
+                //}
             }
-            //}
         }
     }
     void Charge()
     {
-        robotMovement.SetTarget(hedgehog.transform.position);
-        timeSinceCharging += Time.deltaTime;
-
-        //Vector3 hedgeHogPosition = new Vector3(hedgehog.transform.position.x, hedgehog.transform.position.y, transform.position.z);
-        //transform.position = Vector3.MoveTowards(transform.position, hedgeHogPosition, speed / 2 * Time.deltaTime);
-        //transform.rotation = Quaternion.FromToRotation(transform.position, hedgeHogPosition - transform.position);
-
-        Wings.transform.localEulerAngles = new Vector3(0, 0, Wings.transform.localEulerAngles.z + 45 / chargeDuration * Time.deltaTime);
-
-        if (timeSinceCharging > chargeDuration)
+        if (hedgehog != null)
         {
-            robotMovement.movementSpeed = 0;
-            state = BatterState.Batting;
-            timeSinceBatting = 0;
+            robotMovement.SetTarget(hedgehog.transform.position);
+            timeSinceCharging += Time.deltaTime;
+
+            //Vector3 hedgeHogPosition = new Vector3(hedgehog.transform.position.x, hedgehog.transform.position.y, transform.position.z);
+            //transform.position = Vector3.MoveTowards(transform.position, hedgeHogPosition, speed / 2 * Time.deltaTime);
+            //transform.rotation = Quaternion.FromToRotation(transform.position, hedgeHogPosition - transform.position);
+
+            Wings.transform.localEulerAngles = new Vector3(0, 0, Wings.transform.localEulerAngles.z + 45 / chargeDuration * Time.deltaTime);
+
+            if (timeSinceCharging > chargeDuration)
+            {
+                robotMovement.movementSpeed = 0;
+                state = BatterState.Batting;
+                timeSinceBatting = 0;
+            }
         }
     }
 
@@ -87,27 +110,31 @@ public class BatterBot : MonoBehaviour
         Wings.transform.localEulerAngles = new Vector3(0, 0, Wings.transform.localEulerAngles.z - 360 / batDuration * Time.deltaTime);
         timeSinceBatting += Time.deltaTime;
 
-        bool bonked = false;
-
-        if (batCollider.IsTouching(hedgehogCollider))
+        if (hedgehog != null)
         {
-            float angle = BatObject.transform.rotation.eulerAngles.z / 180 * Mathf.PI;
+            bool bonked = false;
 
-            hedgehogRB.velocity = new Vector2(Mathf.Cos(angle + Mathf.PI / 2), Mathf.Sin(angle + Mathf.PI / 2)) * knockback;
-            bonked = true;
-            hedgehog.DropEmeralds();
-        }
+            if (batCollider.IsTouching(hedgehogCollider))
+            {
+                float angle = BatObject.transform.rotation.eulerAngles.z / 180 * Mathf.PI;
 
-        if (timeSinceBatting > batDuration || bonked)
-        {
-            state = BatterState.Moving;
-            robotMovement.movementSpeed = speed;
+                hedgehogRB.velocity = new Vector2(Mathf.Cos(angle + Mathf.PI / 2), Mathf.Sin(angle + Mathf.PI / 2)) * knockback;
+                bonked = true;
+                hedgehog.DropEmeralds();
+            }
+
+            if (timeSinceBatting > batDuration || bonked)
+            {
+                state = BatterState.Moving;
+                robotMovement.movementSpeed = speed;
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindHedgehog();
         switch (state)
         {
             case BatterState.Moving:
